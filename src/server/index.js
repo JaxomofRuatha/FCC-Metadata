b0ead650db41982143cca345e83aba9a8110d978
John Chritton <jchritton33@gmail.com>
2018-02-05 18:06:48 -0600

    First commit

---
+++ src/server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const env = require('../config/env');
+
+const index = require('./routes');
+
+const app = express();
+
+//Port setup
+const port = process.env.PORT || 8080;
+
+//Set up path for views and view engine
+app.set('views', path.join(__dirname, '../client/views'));
+app.set('view engine', 'pug');
+
+//Set up directory for static served files
+app.use(express.static(path.join(__dirname, '../../public')));
+
+//Set up index as router on root
+app.use('/', index);
+
+//Pass error to error handler for rendering 404.
+app.use((req, res, next) => {
+  let error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+//Error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  //Render error page based on passed status
+  res.status(err.status || 500);
+  res.render('error404');
+});
+
+//Start server
+app.listen(port, () => {
+  console.log('File Metadata server is listening on port ' + port);
+});
